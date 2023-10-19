aef26c59fda007962187d9e31c508329a0866452
tomstehling <stehling.tom@gmail.com>
2023-10-19 16:29:43 +0200

    contentful caching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  // build: {
-  //   transpile: ["naive-ui", "vueuc", "contentful"],
-  // },
+  modules: ["@kevinmarrec/nuxt-pwa"],
+  pwa: {
+    meta: {
+      name: "Wurzelfestival",
+      description: "Zurueck zu den Wurzeln",
+      theme_color: "#aa22ff",
+    },
+    workbox: {
+      templatePath: "./sw.js",
+    },
+  },
+
+  build: {
+    transpile: ["naive-ui", "vueuc", "contentful"],
+  },
 });
