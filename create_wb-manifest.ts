4e5f32c3191eb11e5bb2a76c7383ace3260d59a9
tomstehling <stehling.tom@gmail.com>
2023-11-29 10:19:21 +0100

    implement precaching

---
+++ create_wb-manifest.ts
@@ -0,0 +1,20 @@
+interface File {
+  revision: string;
+  url: string;
+}
+function randomString(length: number) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  return Array.from({ length }, () =>
+    characters.charAt(Math.floor(Math.random() * characters.length))
+  ).join("");
+}
+
+export const wb_manifest: File[] = [];
+export function create_wb_manifest(manifestArr: string[]): File[] {
+  manifestArr.map((path) => {
+    wb_manifest.push({ revision: randomString(12), url: `_nuxt/${path}` });
+  });
+  console.log("creat", wb_manifest);
+  return wb_manifest;
+}
--- pages/index.vue
+++ pages/index.vue
@@ -24,3 +24,49 @@
     >
   </n-space>
 </template>
+
+<script setup>
+import { getEntries } from "../contentful/contentfulAPI";
+import { inject, provide } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+let isLoading = ref(true);
+const showWishlist = ref(route.query.showWishlist);
+const lineup = reactive([]);
+const festivalDates = reactive([]);
+const floors = reactive([]);
+
+const getLineup = async () => {
+  if ("serviceWorker" in navigator) {
+    const status = await navigator.serviceWorker.ready;
+    console.log("service-worker ready", status);
+    try {
+      console.log("start fetching");
+      //making the fist fetch twice, because otherwise the sw cant hook up on it.dont know why, but wont work otherwise
+      const notDays = await getEntries({ content_type: "tage" });
+      const days = await getEntries({ content_type: "tage" });
+      const stages = await getEntries({ content_type: "floors" });
+      const combinedTimetable = await getEntries({
+        content_type: "zeitplan",
+      });
+      console.log("zeitplan:", combinedTimetable);
+
+      combinedTimetable.map((e) => {
+        lineup.push(e);
+      });
+      days.map((e) => {
+        festivalDates.push(e);
+      });
+      stages.map((e) => {
+        floors.push(e);
+      });
+    } finally {
+      console.log("data fetch finished");
+    }
+  }
+};
+
+onMounted(() => {
+  getLineup();
+});
+</script>
